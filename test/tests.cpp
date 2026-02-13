57b4a10288ae9a16957d2f052d6a54585f60493b
Fraser Watt <fsrwatt@gmail.com>
2026-02-13 20:32:07 +0000

    More moving to GoogleTest
    Had some tests still using the old format

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,14 +1,8 @@
-#include <catch2/catch_test_macros.hpp>
+#include <gtest/gtest.h>
 
-
-#include <myproject/sample_library.hpp>
-
-
-TEST_CASE("Factorials are computed", "[factorial]")
-{
-  REQUIRE(factorial(0) == 1);
-  REQUIRE(factorial(1) == 1);
-  REQUIRE(factorial(2) == 2);
-  REQUIRE(factorial(3) == 6);
-  REQUIRE(factorial(10) == 3628800);
+TEST(HelloTest, BasicAssertions) {
+  // Expect two strings to not be equal
+  EXPECT_STRNE("hello", "world");
+  // Expect equality
+  EXPECT_EQ(1, 2-1);
 }
